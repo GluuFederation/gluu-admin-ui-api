94aa22e291b2fa4a2315678eeab1332d1cfb4a00
Arnab Dutta <arnab.bdutta@gmail.com>
2021-02-24 12:04:16 +0530

    Fixing test cases

--- app/src/test/java/org/gluu/adminui/app/controllers/OAuth2ControllerTest.java
+++ app/src/test/java/org/gluu/adminui/app/controllers/OAuth2ControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -24,6 +25,7 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class OAuth2ControllerTest {
 
