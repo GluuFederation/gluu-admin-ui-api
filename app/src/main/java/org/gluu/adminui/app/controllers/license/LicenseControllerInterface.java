c332fbc3a734f239af93ab4264ed285fb8127fe2
Arnab Dutta <arnab.bdutta@gmail.com>
2021-07-03 22:45:45 +0530

    feat: license enforcement #80

--- app/src/main/java/org/gluu/adminui/app/controllers/license/LicenseControllerInterface.java
+++ app/src/main/java/org/gluu/adminui/app/controllers/license/LicenseControllerInterface.java
@@ -2,7 +2,7 @@ package org.gluu.adminui.app.controllers.license;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface LicenseControllerInterface {
     String CHECK_LICENSE = "/license/checkLicense";
@@ -11,6 +11,6 @@ public interface LicenseControllerInterface {
     @GetMapping(CHECK_LICENSE)
     Boolean checkLicense();
 
-    @PostMapping(ACTIVATE_LICENSE)
-    Boolean activateLicense(@RequestBody String licenseKey) throws Exception;
+    @GetMapping(ACTIVATE_LICENSE)
+    Boolean activateLicense(@RequestParam String licenseKey) throws Exception;
 }
