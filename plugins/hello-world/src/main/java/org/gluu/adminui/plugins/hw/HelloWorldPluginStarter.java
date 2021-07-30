6874d33c2328c38fb491a3e3770d466e7f989791
Arnab Dutta <arnab.bdutta@gmail.com>
2021-07-30 21:37:42 +0530

    feat: hello world plugin

---
+++ plugins/hello-world/src/main/java/org/gluu/adminui/plugins/hw/HelloWorldPluginStarter.java
@@ -0,0 +1,11 @@
+package org.gluu.adminui.plugins.hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldPluginStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldPluginStarter.class, args);
+    }
+}
